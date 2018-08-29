7084730ac5c4188faff9d0b4cb3c1ab4f71d4453
Yumainaura <yuma.inaura@gmail.com>
2018-08-29 18:16:49 +0900

    update install script

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ if [ ! $(which ruby) ]; then echo you need ruby && exit 1; fi
 if [ ! $(which gist) ]; then
   echo you need gist command && exit 1
   gem install gist
+  gist --login
 fi
 
 cp -f ./bin/gistnow /usr/local/bin
